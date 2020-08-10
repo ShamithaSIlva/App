745bc7c5da9a8b270c8b1bf2625669563652d215
ShamithaSilva <shamithadevinda@gmail.com>
2020-08-10 21:47:20 +0100

    Adding SpringBoot starter files

---
+++ src/main/java/com/threefold/crud/SpringCrudAppThreeFoldApplication.java
@@ -0,0 +1,13 @@
+package com.threefold.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudAppThreeFoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudAppThreeFoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threefold/crud/SpringCrudAppThreeFoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threefold.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudAppThreeFoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
